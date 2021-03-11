98b5d5c93b4b1f38c1d106a9a5e205f842361aee
Sandeep <itssharmasandeep@gmail.com>
2021-03-11 13:48:58 +0530

    Adding comments file to show how to write single line and multi line comment

---
+++ Comments.py
@@ -0,0 +1,12 @@
+# This is to show single line and multi line comment
+# Created By: Sandeep Sharma 11-Mar-2021
+
+# This is single line comment
+
+'''
+This
+is
+multiple
+line 
+comment
+'''
--- Hello_World.py
+++ Hello_World.py
@@ -1,4 +1,5 @@
 # This is the first code in python
+# Created By: Sandeep Sharma 11-Mar-2021
 
 # To import any module in python
 import os      # If any module is not present, then it will give an error like this => ## No module named <Module_Name> ##
