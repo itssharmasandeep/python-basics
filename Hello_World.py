4218807c786da61d92a7efaf67c1d9731aa2d4f2
Sandeep <itssharmasandeep@gmail.com>
2021-03-11 13:39:40 +0530

    Importing modules and printing hello world

---
+++ Hello_World.py
@@ -0,0 +1,7 @@
+# This is the first code in python
+
+# To import any module in python
+import os      # If any module is not present, then it will give an error like this => ## No module named <Module_Name> ##
+
+# Printing hello world
+print("Hello World")
